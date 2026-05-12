3020d7b8bfb4e8fef84053803f339c5555881548
MingZhi Liu <xichen.dev@gmail.com>
2026-05-12 11:56:15 +0800

    Change namespace from 'transform' to 'view'

--- lumen-transform/build.gradle.kts
+++ lumen-transform/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.xichen.lumen.transform"
+    namespace = "com.xichen.lumen.view"
     compileSdk {
         version = release(36)
     }
@@ -37,7 +37,11 @@ android {
 dependencies {
     api(platform(project(":lumen")))
     api(project(":lumen-core"))
+    api(project(":lumen-transform"))
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.kotlinx.coroutines.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
